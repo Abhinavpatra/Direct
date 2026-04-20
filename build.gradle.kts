b40de114492f2242ebeb4f38e3034ad5188e963b
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2026-04-20 17:53:40 +0530

    remoced it from the main part for firebase issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,4 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.6" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
-    id("com.google.gms.google-services") version "4.4.4" apply false
 }
